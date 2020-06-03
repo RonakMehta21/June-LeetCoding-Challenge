e314d464a87bad9f4b436055a6b1902100ba51fc
Ronak Mehta <ronakmehta21@gmail.com>
2020-06-02 23:52:08 -0700

    Update DeleteNodeInALinkedList.py

--- Week1/DeleteNodeInALinkedList.py
+++ Week1/DeleteNodeInALinkedList.py
@@ -10,18 +10,5 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        start = node
-        end = node
-        start = start.next
-        l1=[]
-        while start:
-            l1.append(start.val)
-            start = start.next
-        for x in range(len(l1)):
-            node.val = l1[x]
-            node = node.next
-        prev = None
-        while end.next!=None:
-            prev = end
-            end = end.next
-        prev.next = None
+        node.val = node.next.val
+        node.next = node.next.next
